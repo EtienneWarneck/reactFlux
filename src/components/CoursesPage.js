c8a3859661fe3dbf9c9ac15e80b37a55d77dc040
Etienne Warneck <etienne.warneck@gmail.com>
2022-04-04 15:58:13 -0400

    Add Form and handle changes

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { getCourses } from "../api/courseApi";
 import CourseList from "./CourseList";
 
 function CoursesPage() {
     const [courses, setCourses] = useState([]);
+
+
 
     useEffect(() => {
         getCourses().then(_courses => setCourses(_courses));
@@ -12,6 +15,9 @@ function CoursesPage() {
     return (
         <>
             <h2>Courses</h2>
+            <Link className="btn btn-primary" to="/course">
+                Add course
+            </Link>
             <CourseList courses={courses} />
         </>
     );
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,19 +1,34 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+// import { useParams } from "react-router-dom";
+import CourseForm from "./CourseForm";
 
-export default function ManageCoursePage() {
+const ManageCoursePage = props => {
     // console.log("props.match.params.slug", props.match.params.slug)
-    const { slug } = useParams()
+    // const { slug } = useParams()
+    const [course, setCourse] = useState({
+        id: null,
+        slug: "",
+        authorId: null,
+        category: ""
+    })
+
+    function handleChange({ target }) {
+        //debugger
+        setCourse({
+            ...course,
+            [target.id]: target.value
+        })
+    }
 
     return (
         <>
             <h2>Manage Course</h2>
             {/* {props.match.params.slug} */}
-            <h3>
-                {slug}
-            </h3>
+            {/* {slug} */}
+            <CourseForm course={course} onChange={handleChange} />
         </>
     );
 };
 
 
+export default ManageCoursePage
